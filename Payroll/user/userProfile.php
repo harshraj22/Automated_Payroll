a0ab85580288bb98337f06eb8c663cf0de23ca98
harshraj22 <harshraj22aug@gmail.com>
2019-10-22 23:39:17 +0530

    added user profile

--- Payroll/user/userProfile.php
+++ Payroll/user/userProfile.php
@@ -2,12 +2,35 @@
     session_start();
     require_once "../authenticate/login.php";
 
-    if(!isset($_SESSION['queriedUser']) || !isset($_SESSION['isAdmin']) || $_SESSION['isAdmin'] == false){
-        die("Error 404 <br> The page you requested doesn't exist.");
-        header('Refresh:01; url=../index.php');
+    if(!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] == false){
+        echo "Error 404. The page you requested doesn't exists. ".isset($_SESSION['loggedIn'])." and {$_SESSION['loggedIn']}";
+        header("Refresh:02; url=../index.php");
         exit();
     }
 
-    
+    echo "Latitude : {$_GET['latitude']}<br> Longitude : {$_GET['longitude']}<br>";
 
+    $latitude = trim($_GET['latitude']);
+    $longitude = trim($_GET['longitude']);
+
+    $date = date('Y-n-d');
+    echo $date;
+
+    $conn = mysqli_connect($hostname, $username, $password, $database);
+    if(!$conn)
+        echo "Connection error. Details can't be updated.";
+    else{
+        $curUserName = trim($_SESSION['user']);
+        $user_update_query = "INSERT INTO {$curUserName} VALUES('{$date}','{$latitude}','{$longitude}','NULL')";
+        $user_update_result = mysqli_query($conn, $user_update_query);
+
+        if(!$user_update_result)
+            echo "Error updating the user details.";
+        else 
+            echo "Successfully updated the user details.";
+    }
+
+    header('Refresh:10; url=userProfileFrontend.php');
+    // echo "asldfja asdlf";
+    exit();
 ?>
---
+++ Payroll/user/userProfileFrontend.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>EXclamation</title>
+</head>
+<body>
+    <script type='text/javascript' src="userProfile.js"></script>
+
+    <form type='post' enctype="multipart/form-data" action='userProfile.php'>
+        <input type="text" name='latitude' id='latitude'>
+        <input type="text" name='longitude' id='longitude'>
+        <button type='submit' id='buttonId' >Submit</button>
+    </form>
+    
+    
+</body>
+</html>
