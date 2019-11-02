80d4c4a5d1456af866f22905ce34e90a2dba1a77
abhiisshheekk <abhiisshheekk@gmail.com>
2019-11-03 00:53:24 +0530

    added nav bar to createUser and createHr page

--- Payroll/admin/createUser.php
+++ Payroll/admin/createUser.php
@@ -57,6 +57,31 @@
 				<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<a class="navbar-brand" href="index.html">Home</a>
+
+				<div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+					<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+					<li class="nav-item active">
+						<a class="nav-link" href="https://github.com/harshraj22/Automated_Payroll">Git <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="../user/logout.php">Logout</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled" href="#">Disabled</a>
+					</li>
+					</ul>
+					<form class="form-inline my-2 my-lg-0" action="https://www.google.com">
+					<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+					</form>
+				</div>
+				</nav>
+
 				<body style="background: linear-gradient(to bottom, #33ccff 0%, #ff99cc 100%);">
 					<div class="signupSection">
 					  <div class="info">
